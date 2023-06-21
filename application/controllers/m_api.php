804872777e97e9bffc9567cff6fea2890065c186
Litbang Bungamayang <litbang.bungamayang@gmail.com>
2023-06-21 10:49:21 +0700

    [+] Ubah tes api lagi

--- application/controllers/m_api.php
+++ application/controllers/m_api.php
@@ -49,7 +49,7 @@ class M_api extends SB_Controller
 	function tes_api(){
 		$payload = $this->input->get('payload');
 		$this->load->model('mobilemodel');
-		var_dump (json_decode($this->mobilemodel->tes_api($payload)));
+		echo (json_decode($this->mobilemodel->tes_api($payload)));
 	}
 
 	function truk(){
