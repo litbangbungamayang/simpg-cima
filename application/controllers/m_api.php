a021e20c17555b450a3c719f26ad18436994329d
Litbang Bungamayang <litbang.bungamayang@gmail.com>
2023-06-22 00:09:00 +0700

    [+] Ganti header ke json

--- application/controllers/m_api.php
+++ application/controllers/m_api.php
@@ -49,6 +49,7 @@ class M_api extends SB_Controller
 	function tes_api(){
 		$payload = $this->input->get('payload');
 		$this->load->model('mobilemodel');
+		header("Content-Type: application/json");
 		echo $this->mobilemodel->tes_api($payload);
 	}
 
