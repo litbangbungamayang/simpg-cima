7a7beeeb6af654f9e10e0ea70377d54d2a61037c
Litbang Bungamayang <litbang.bungamayang@gmail.com>
2023-06-22 09:33:24 +0700

    [+] get post

--- application/controllers/m_api.php
+++ application/controllers/m_api.php
@@ -47,7 +47,7 @@ class M_api extends SB_Controller
 	}
 
 	function tes_api(){
-		$payload = $this->input->get('payload');
+		$payload = $this->input->getPost('payload');
 		$this->load->model('mobilemodel');
 		/*
 		if($this->mobilemodel->tes_api($payload)){
